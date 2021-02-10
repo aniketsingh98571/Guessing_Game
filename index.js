1b2b4e81f072a155da8e76c3fc514dd01b15635d
Aniket Singh <thor98571@gmail.com>
2021-02-10 22:59:13 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,9 +2,13 @@ let inputd=document.getElementById("input");
 let generate=document.getElementById("checkd");
 
 function check(){
+    if(inputd.value===""){
+        generate.innerHTML="Enter the value";
+    }
+    else{
     let random=Math.floor(Math.random() * 100) + 1;
     if(inputd.value==random)
         generate.innerHTML="👍";
     else
-    generate.innerHTML="👎 Try Again";
+    generate.innerHTML="👎 Try Again";}
 }
