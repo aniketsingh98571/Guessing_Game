1683e39b9b7c4e54498f7e6e9f4c3b157a2aa93b
Aniket Singh <thor98571@gmail.com>
2021-02-12 21:59:24 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 let inputd=document.getElementById("input");
 let generate=document.getElementById("checkd");
-
+let loader=document.getElementById("loading");
 function check(){
     if(inputd.value===""){
         generate.innerHTML="Enter the value";
@@ -11,4 +11,7 @@ function check(){
         generate.innerHTML="👍";
     else
     generate.innerHTML="👎 Try Again";}
+}
+function callFunction(){
+    loader.style.display="none";
 }
